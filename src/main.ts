88d34052a19a98fe79a893c4b74c770786716754
lvhaizhou <417703682@qq.com>
2022-04-20 09:57:08 +0800

    refactor(renderer): 代码重构

--- src/main.ts
+++ src/main.ts
@@ -6,69 +6,8 @@
 // app.use(router);
 // app.mount("#app");
 
-import { Application, Container, Sprite, Text, Texture } from "pixi.js";
+import { game } from "./game";
 import App from "./App.vue";
+import { createApp } from "./runtime-canvas";
 
-import { createRenderer } from "vue";
-
-const game = new Application({
-  width: 500,
-  height: 500,
-});
-
-document.body.appendChild(game.view);
-
-const renderer = createRenderer<Container, Container>({
-  createElement(type) {
-    // 容器：Container
-    let element;
-    switch (type) {
-      case "Container":
-        element = new Container();
-        break;
-      case "Sprite":
-        element = new Sprite();
-        break;
-      default:
-        throw new Error(`Unknown element type: ${type}`);
-    }
-    return element;
-  },
-  patchProp(el, key, prevValue, nextValue) {
-    switch (key) {
-      case "texture":
-        (el as Sprite).texture = Texture.from(nextValue);
-        break;
-      default:
-        break;
-    }
-  },
-  insert(el, parent) {
-    if (el && parent) {
-      parent.addChild(el);
-    }
-  },
-  remove(el) {
-    if (el && el.parent) {
-      el.parent.removeChild(el);
-    }
-  },
-  createText(text) {
-    return new Text(text);
-  },
-  createComment(text) {
-    return new Text(text);
-  },
-  setText() {
-  },
-  setElementText() {
-  },
-  parentNode(node) {
-    return node.parent;
-  },
-  nextSibling() {
-    return null;
-  },
-});
-
-renderer.createApp(App).mount(game.stage);
+createApp(App).mount(game.stage);
