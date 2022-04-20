760236b95fcdb8ef9ac21e3a700561e718e048cb
lvhaizhou <417703682@qq.com>
2022-04-20 09:49:17 +0800

    feat(app): 实现渲染器

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Container><Sprite :texture="logo" /></Container>
+</template>
+
+<script setup lang="ts">
+import "./style/common.scss";
+import logo from "./assets/logo.png";
+</script>
+
+<style>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@
 // app.use(router);
 // app.mount("#app");
 
-import { Application } from "pixi.js";
+import { Application, Container, Sprite, Text, Texture } from "pixi.js";
+import App from "./App.vue";
+
+import { createRenderer } from "vue";
 
 const game = new Application({
   width: 500,
@@ -14,3 +17,58 @@ const game = new Application({
 });
 
 document.body.appendChild(game.view);
+
+const renderer = createRenderer<Container, Container>({
+  createElement(type) {
+    // 容器：Container
+    let element;
+    switch (type) {
+      case "Container":
+        element = new Container();
+        break;
+      case "Sprite":
+        element = new Sprite();
+        break;
+      default:
+        throw new Error(`Unknown element type: ${type}`);
+    }
+    return element;
+  },
+  patchProp(el, key, prevValue, nextValue) {
+    switch (key) {
+      case "texture":
+        (el as Sprite).texture = Texture.from(nextValue);
+        break;
+      default:
+        break;
+    }
+  },
+  insert(el, parent) {
+    if (el && parent) {
+      parent.addChild(el);
+    }
+  },
+  remove(el) {
+    if (el && el.parent) {
+      el.parent.removeChild(el);
+    }
+  },
+  createText(text) {
+    return new Text(text);
+  },
+  createComment(text) {
+    return new Text(text);
+  },
+  setText() {
+  },
+  setElementText() {
+  },
+  parentNode(node) {
+    return node.parent;
+  },
+  nextSibling() {
+    return null;
+  },
+});
+
+renderer.createApp(App).mount(game.stage);
